7d91913c95bcc73b67b81c09b9f95cdbf198a7de
mpautasso <manuelpautasso@gmail.com>
2022-12-05 10:43:30 -0300

    Added authentication controller

---
+++ src/main/java/com/mpautasso/controller/AuthenticationController.java
@@ -0,0 +1,35 @@
+package com.mpautasso.controller;
+
+import com.mpautasso.dto.AuthRequest;
+import com.mpautasso.dto.UsuarioRegisterResponse;
+import com.mpautasso.dto.UsuarioRequest;
+import com.mpautasso.model.Usuario;
+import com.mpautasso.security.MyAuthenticationManager;
+import com.mpautasso.service.Implementation.UsuarioServiceImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/auth")
+@Slf4j
+public class AuthenticationController {
+    private final UsuarioServiceImpl usuarioService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registrarUsuario(@RequestBody UsuarioRequest usuarioRequest){
+        UsuarioRegisterResponse usuario = usuarioService.crearUsuario(usuarioRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loguearUsuario(@RequestBody AuthRequest authRequest){
+        return ResponseEntity.ok(usuarioService.loguearUsuario(authRequest));
+    }
+}
