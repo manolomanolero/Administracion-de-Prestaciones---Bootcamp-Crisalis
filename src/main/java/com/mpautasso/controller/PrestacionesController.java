75c2165ee5a3d5c8a43e8a6e6cd62ad906fc00de
mpautasso <manuelpautasso@gmail.com>
2022-12-21 12:45:03 -0300

    Added create Servicio and delete Prestacion endpoints

--- src/main/java/com/mpautasso/controller/PrestacionesController.java
+++ src/main/java/com/mpautasso/controller/PrestacionesController.java
@@ -1,6 +1,8 @@
 package com.mpautasso.controller;
 
+import com.mpautasso.dto.PrestacionResponse;
 import com.mpautasso.dto.ProductoRequest;
+import com.mpautasso.dto.ServicioRequest;
 import com.mpautasso.model.Prestacion;
 import com.mpautasso.service.PrestacionesService;
 import lombok.RequiredArgsConstructor;
@@ -17,12 +19,24 @@ public class PrestacionesController {
     private final PrestacionesService prestacionesService;
 
     @GetMapping
-    public ResponseEntity<List<Prestacion>> listarPrestaciones(){
+    public ResponseEntity<List<PrestacionResponse>> listarPrestaciones(){
         return ResponseEntity.ok(prestacionesService.listarPrestaciones());
     }
 
     @PostMapping("/productos")
-    public ResponseEntity<Prestacion> crearProducto(@RequestBody ProductoRequest productoRequest){
-        return ResponseEntity.status(HttpStatus.CREATED).body(prestacionesService.crearProducto(productoRequest));
+    public ResponseEntity<PrestacionResponse> crearProducto(@RequestBody ProductoRequest productoRequest){
+        return ResponseEntity.status(HttpStatus.CREATED).body(prestacionesService.crearPrestacion(productoRequest));
     }
+    @PostMapping("/servicios")
+    public ResponseEntity<PrestacionResponse> crearServicio(@RequestBody ServicioRequest servicioRequest){
+        return ResponseEntity.status(HttpStatus.CREATED).body(prestacionesService.crearPrestacion(servicioRequest));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> eliminarPrestacion(@RequestParam String nombre){
+        prestacionesService.eliminarPrestacion(nombre);
+        return ResponseEntity.status(HttpStatus.OK).body("Borrado de la prestacion " + nombre +" con exito");
+    }
+
 }
+
