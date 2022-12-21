40648418e26f69241add2bb75260769d0b2eaead
mpautasso <manuelpautasso@gmail.com>
2022-12-21 12:41:21 -0300

    Added queries to find and search if exists, by nombre

--- src/main/java/com/mpautasso/repository/PrestacionesRepository.java
+++ src/main/java/com/mpautasso/repository/PrestacionesRepository.java
@@ -3,5 +3,9 @@ package com.mpautasso.repository;
 import com.mpautasso.model.Prestacion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PrestacionesRepository extends JpaRepository<Prestacion, Long> {
+    Optional<Prestacion> findByNombre(String nombre);
+    boolean existsByNombre(String nombre);
 }
