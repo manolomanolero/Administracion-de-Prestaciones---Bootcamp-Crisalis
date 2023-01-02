ad7aad83ed27c34072d6808ce2e94a0665428955
mpautasso <manuelpautasso@gmail.com>
2023-01-02 15:33:40 -0300

    Added empresa and clientes endpoints

--- src/main/java/com/mpautasso/controller/ClienteController.java
+++ src/main/java/com/mpautasso/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.mpautasso.controller;
 
+import com.mpautasso.dto.StringResponse;
 import com.mpautasso.dto.cliente.ClienteRequest;
 import com.mpautasso.dto.cliente.ClienteResponse;
+import com.mpautasso.dto.cliente.ClienteUpdateRequest;
 import com.mpautasso.dto.empresa.EmpresaRequest;
 import com.mpautasso.dto.empresa.EmpresaResponse;
 import com.mpautasso.service.ClientesService;
@@ -26,9 +28,25 @@ public class ClienteController {
         return ResponseEntity.ok(clientesService.listarClientes());
     }
 
+    @GetMapping("/search")
+    public ResponseEntity<ClienteResponse> buscarCliente(@RequestParam Long id){
+        return ResponseEntity.ok(clientesService.buscarCliente(id));
+    }
+
     @PostMapping
     public ResponseEntity<ClienteResponse> crearCliente(@RequestBody ClienteRequest clienteRequest){
         return ResponseEntity.status(HttpStatus.CREATED).body(clientesService.crearCliente(clienteRequest));
+    }
+
+    @PutMapping
+    public ResponseEntity<ClienteResponse> actualizarCliente(@RequestBody ClienteUpdateRequest clienteRequest){
+        return ResponseEntity.ok(clientesService.editarCliente(clienteRequest));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<StringResponse> eliminarCliente(@RequestParam Long id){
+        clientesService.eliminarCliente(id);
+        return ResponseEntity.ok(new StringResponse("Borrado del Cliente con exito"));
     }
 
 
@@ -37,8 +55,24 @@ public class ClienteController {
         return ResponseEntity.ok(empresaService.listarEmpresas());
     }
 
+    @GetMapping("/empresas/search")
+    public ResponseEntity<EmpresaResponse> buscarEmpresa(@RequestParam Long id){
+        return ResponseEntity.ok(empresaService.buscarEmpresaPorId(id));
+    }
+
     @PostMapping("/empresas")
     public ResponseEntity<EmpresaResponse> crearEmpresa(@RequestBody EmpresaRequest empresaRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(empresaService.crearEmpresa(empresaRequest));
+    }
+
+    @PutMapping("/empresas")
+    public ResponseEntity<EmpresaResponse> actualizarEmpresa(@RequestBody EmpresaRequest empresaRequest) {
+        return ResponseEntity.ok(empresaService.editarEmpresa(empresaRequest));
+    }
+
+    @DeleteMapping("/empresas")
+    public ResponseEntity<StringResponse> eliminarEmpresa(@RequestParam Long id){
+        empresaService.eliminarEmpresa(id);
+        return ResponseEntity.ok(new StringResponse("Borrado de la Empresa con exito"));
     }
 }
