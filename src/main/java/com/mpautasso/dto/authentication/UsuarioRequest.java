9b79b31e7c72f23ba1f2a194562585aba4666106
mpautasso <manuelpautasso@gmail.com>
2023-01-30 08:45:59 -0300

    Added constructor to UsuarioRequest dto

--- src/main/java/com/mpautasso/dto/authentication/UsuarioRequest.java
+++ src/main/java/com/mpautasso/dto/authentication/UsuarioRequest.java
@@ -1,8 +1,12 @@
 package com.mpautasso.dto.authentication;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UsuarioRequest {
     private String username;
     private String password;
