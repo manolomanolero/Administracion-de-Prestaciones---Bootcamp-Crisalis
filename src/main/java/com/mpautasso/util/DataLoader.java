00cd26b61828c4425f8d884e21a62625e45edda0
mpautasso <manuelpautasso@gmail.com>
2023-01-30 08:49:55 -0300

    Fixed some constructors on dataLoader

--- src/main/java/com/mpautasso/util/DataLoader.java
+++ src/main/java/com/mpautasso/util/DataLoader.java
@@ -1,16 +1,16 @@
 package com.mpautasso.util;
 
+import com.mpautasso.dto.authentication.UsuarioRequest;
 import com.mpautasso.model.*;
 import com.mpautasso.repository.*;
+import com.mpautasso.service.ClientesService;
+import com.mpautasso.service.Implementation.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Component
 public class DataLoader implements ApplicationRunner {
@@ -28,6 +28,10 @@ public class DataLoader implements ApplicationRunner {
     private PedidoRepository pedidoRepository;
     @Autowired
     private DetallesPedidoRepository detallesRepository;
+    @Autowired
+    private UsuarioServiceImpl usuarioServiceImpl;
+    @Autowired
+    private ClientesServiciosContratadosRepository serviciosContratadosRepository;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -41,17 +45,26 @@ public class DataLoader implements ApplicationRunner {
 
             }
 
+            List<Usuario> usuarioList = usuarioServiceImpl.listarUsuarios();
+
+            if(usuarioList.size() < 2){
+                UsuarioRequest usuario1 = new UsuarioRequest("user", "user", "user@gmail.com");
+                UsuarioRequest usuario2 = new UsuarioRequest("carlos", "123", "carlos@gmail.com");
+                usuarioServiceImpl.crearUsuario(usuario1);
+                usuarioServiceImpl.crearUsuario(usuario2);
+            }
+
             List<Prestacion> prestaciones = prestacionesRepository.findAll();
 
             if(prestaciones.size() < 5){
                 Prestacion prestacion1 = prestacionesRepository.save(new Producto(1L, "fideos", 160d));
-                Prestacion prestacion2 = prestacionesRepository.save(new Servicio(2L, "internet", 2300d));
+                Prestacion prestacion2 = prestacionesRepository.save(new Servicio(2L, "internet", 2300d, 1000d));
                 Prestacion prestacion3 = prestacionesRepository.save(new Producto(3L, "carne", 999d));
-                Prestacion prestacion4 = prestacionesRepository.save(new Servicio(4L, "agua", 1100d));
+                Prestacion prestacion4 = prestacionesRepository.save(new Servicio(4L, "agua", 1100d, 0d));
                 Prestacion prestacion5 = prestacionesRepository.save(new Producto(5L, "pollo", 1110d));
-                Prestacion prestacion6 = prestacionesRepository.save(new Servicio(6L, "luz", 1500d));
+                Prestacion prestacion6 = prestacionesRepository.save(new Servicio(6L, "luz", 1500d, 0d));
                 Prestacion prestacion7 = prestacionesRepository.save(new Producto(7L, "aceite", 458d));
-                Prestacion prestacion8 = prestacionesRepository.save(new Servicio(8L, "cable", 1100d));
+                Prestacion prestacion8 = prestacionesRepository.save(new Servicio(8L, "cable", 1100d, 800d));
             }
 
             List<Impuesto> impuestos = impuestosRepository.findAll();
@@ -100,36 +113,47 @@ public class DataLoader implements ApplicationRunner {
             if(pedidos.size() < 3){
                 Impuesto impuesto1 = impuestosRepository.findById(1L).get();
                 Impuesto impuesto2 = impuestosRepository.findById(2L).get();
-                List<Impuesto> impuestos1 = new ArrayList<>();
+                Set<Impuesto> impuestos1 = new HashSet<>();
                 impuestos1.add(impuesto1);
                 impuestos1.add(impuesto2);
 
 
                 DetallesPedido detalles1 = new DetallesPedido(null, 1,
-                        prestacionesRepository.findById(1L).get(), impuestos1,0);
+                        prestacionesRepository.findById(1L).get(), impuestos1,0, false);
                 DetallesPedido detalles2 = new DetallesPedido(null, 1,
-                        prestacionesRepository.findById(2L).get(), impuestos1,0);
+                        prestacionesRepository.findById(2L).get(), impuestos1,0, true);
                 DetallesPedido detalles3 = new DetallesPedido(null, 1,
-                        prestacionesRepository.findById(3L).get(), impuestos1,0);
+                        prestacionesRepository.findById(3L).get(), impuestos1,0, false);
                 DetallesPedido detalles4 = new DetallesPedido(null, 1,
-                        prestacionesRepository.findById(4L).get(), impuestos1,0);
+                        prestacionesRepository.findById(4L).get(), impuestos1,0, false);
 
 
                 Pedido pedido1 = pedidoRepository.save(new Pedido(
                                     clientesRepository.findById(1L).get(),
-                                    new ArrayList<>(List.of(detalles1))
+                                    new HashSet<>(Set.of(detalles1)),
+                        false
                                 ));
                 Pedido pedido2 = pedidoRepository.save(new Pedido(
                         clientesRepository.findById(2L).get(),
-                        new ArrayList<>(Arrays.asList(detalles2, detalles4))
+                        new HashSet<>(Set.of(detalles2, detalles4)),
+                        false
                 ));
 
                 Pedido pedido3 = pedidoRepository.save(new Pedido(
                         clientesRepository.findById(3L).get(),
-                        new ArrayList<>(List.of(detalles3))
+                        new HashSet<>(Set.of(detalles3)),
+                        false
                 ));
-            }
 
+                pedido2.getDetallesPedidoList().stream()
+                        .filter(elem -> elem.getPrestacion().getType().equalsIgnoreCase("servicio"))
+                        .forEach(detallesPedido -> {
+                                ClientesServiciosContratados servicioContratado =
+                                        new ClientesServiciosContratados(null, true, pedido2.getFechaPedido(),
+                                                pedido2.getCliente(), (Servicio) detallesPedido.getPrestacion());
+                                serviciosContratadosRepository.save(servicioContratado);
+                        });
+            }
 
         } catch (Exception e) {
             System.out.println(e);
